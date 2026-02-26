4a780cdf0dfb18cdd213dd52566cc3f4ea464bcf
MarcosPereira97 <marcos_henriquejunior@hotmail.com>
2026-02-25 21:47:13 -0300

    feat: teste usando outros seletores

--- playwright/e2e/pedidos.spec.ts
+++ playwright/e2e/pedidos.spec.ts
@@ -1,20 +1,45 @@
-import { test, expect } from '@playwright/test';
+import { test, expect } from "@playwright/test";
 
-test('has title', async ({ page }) => {
-    await page.goto('/');
-    await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint');
+const orderId = "VLO-MA7H4Y";
 
-    
-    await page.getByRole('link', { name: 'Consultar Pedido' }).click();
-    await expect(page.getByRole('heading')).toContainText('Consultar Pedido');
-    await page.getByTestId('search-order-id').click();
-    await page.getByTestId('search-order-id').fill('VLO-');
-    await page.getByTestId('search-order-id').press('CapsLock');
-    await page.getByTestId('search-order-id').fill('VLO-OHY9BM');
-    await page.getByTestId('search-order-button').click();
-    await expect(page.getByTestId('order-result-id')).toBeVisible();
-    await expect(page.getByTestId('order-result-id')).toContainText('VLO-OHY9BM');
-    await expect(page.getByTestId('order-result-status')).toBeVisible();
-    await expect(page.getByTestId('order-result-status')).toContainText('APROVADO');
+test.skip("deve consultar um pedido aprovado", async ({ page }) => {
+  // Arrange
+  await page.goto("/");
+  await expect(
+    page.getByTestId("hero-section").getByRole("heading"),
+  ).toContainText("Velô Sprint");
+
+  await page.getByRole("link", { name: "Consultar Pedido" }).click();
+  await expect(page.getByRole("heading")).toContainText("Consultar Pedido");
+
+  // Act
+  await page.getByTestId("search-order-id").fill(orderId);
+  await page.getByTestId("search-order-button").click();
+
+  // Assert
+  await expect(page.getByTestId("order-result-id")).toBeVisible({
+    timeout: 10000,
+  });
+  await expect(page.getByTestId("order-result-id")).toContainText(orderId);
+
+  await expect(page.getByTestId("order-result-status")).toBeVisible();
+  await expect(page.getByTestId("order-result-status")).toContainText(
+    "APROVADO",
+  );
 });
 
+test("deve consultar um pedido aprovado sem dataTestId", async ({ page }) => {
+  await page.goto("/");
+  await page.getByRole("link", { name: "Consultar Pedido" }).click();
+
+  await page.getByTestId("search-order-id").fill(orderId);
+  await page.getByRole("button", { name: "Buscar Pedido" }).click();
+  await expect(page.getByText(orderId)).toBeVisible();
+  await expect(page.getByTestId("order-result-VLO-MA7H4Y")).toContainText(
+    orderId,
+  );
+  await expect(page.getByText("APROVADO")).toBeVisible();
+  await expect(page.getByTestId("order-result-VLO-MA7H4Y")).toContainText(
+    "APROVADO",
+  );
+});
