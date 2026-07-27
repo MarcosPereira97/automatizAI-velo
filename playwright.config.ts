2c5758db5682d19500b27078277468fbe1d49c0d
MarcosPereira97 <marcos_henriquejunior@hotmail.com>
2026-07-27 08:01:26 -0300

    fix(test): override stuck terminal env vars with dotenv config

--- playwright.config.ts
+++ playwright.config.ts
@@ -10,8 +10,8 @@ import { fileURLToPath } from "url"
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
-dotenv.config({ path: path.resolve(__dirname, ".env.local") })
-dotenv.config({ path: path.resolve(__dirname, ".env") })
+dotenv.config({ path: path.resolve(__dirname, ".env.local"), override: true })
+dotenv.config({ path: path.resolve(__dirname, ".env"), override: true })
 
 /**
  * See https://playwright.dev/docs/test-configuration.
